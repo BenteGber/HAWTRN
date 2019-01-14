3af91bcf6bd3f9d6b1e525b93dfe10ba4821414f
Bente Gber <benjamingreenberg@Benjamins-MacBook-Pro-2.local>
2019-01-13 20:55:22 -0500

    Pulling adjusted repo

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,19 +1,18 @@
 const log = console.log;
 
 // Yahoo WOEID for Atlanta
-var queryLocation = 2357024;
+var queryLocation = '2357024';
 
-var twitterQueryURL = 'https://cors-anywhere.herokuapp.com/' + 'https://api.twitter.com/1.1/trends/place.json?id=2357024' + 'https://api.twitter.com/1.1/trends/place.json?id=' + queryLocation;
+
+var twitterQueryURL = 'https://cors-anywhere.herokuapp.com/' + 'https://api.twitter.com/1.1/trends/place.json?id=2357024'
+//  + 'https://api.twitter.com/1.1/trends/place.json?id=' + queryLocation + "KVQPwF6rfmHriDZqkmRFStmxA";
 
 $(document).ready(function () {
     $.ajax({
-        url: queryURL,
+        url: twitterQueryURL,
         method: 'GET',
     })
         .then(function (response) {
-
-            var results = response.data();
-
-            log(results);
+            log(response);
         })
 });
